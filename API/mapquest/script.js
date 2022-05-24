ccf4d2ac37e78757d4b35d857ec95b9098b720e0
sarthak <sarthakbatham26@gmail.com>
2022-05-24 18:23:51 +0530

    mapquest

---
+++ API/mapquest/script.js
@@ -0,0 +1,23 @@
+L.mapquest.key = 'qXA40hAdPcrpQ2OXvQAhpvoojIlelrwK';
+
+// 'map' refers to a <div> element with the ID map
+const map = L.mapquest.map('map', {
+  center: [53.480759, -2.242631],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12
+});
+
+map.addControl(L.mapquest.control({position: "topleft"}));
+
+L.marker([53.480759, -2.242631], {
+    icon: L.mapquest.icons.marker({
+      primaryColor: '#22407F',
+      secondaryColor: '#3B5998',
+      shadow: true,
+      size: 'md',
+      symbol: 'A'
+    })
+  })
+  .bindPopup('This is Manchester!')
+  .addTo(map);
+  
